ee93b80cb66abff85e8f9f89c4ba6d35ee7f72e1
jairasolis <jairasolis030@gmail.com>
2023-12-19 11:43:23 +0800

    Landing Page and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 import './App.css'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div>
       <Navbar/>
       <LandingPage/>
+      <Footer/>
     </div>
     
   )
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,6 +7,57 @@ const LandingPage = () => {
         {/* <div className='upang-bg'>
             <img width="1440" height="1032" src="../public/images/upang_bg" alt="" />
         </div> */}
+
+        <div className="box">
+
+
+          <div className="container-1">
+            <div className="line-1">
+              <div className="icon"></div>
+              <div className="line"></div>
+            </div>
+            <div className="text-1">
+              <h3>Create an Account</h3>
+              <p>Create an account to lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
+            </div>
+          </div>
+
+          <div className="container-2">
+            <div className="line-2">
+              <div className="icon"></div>
+              <div className="line"></div>
+            </div>          
+            <div className="text-2">
+              <h3>Get Notified</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
+            </div>
+          </div>
+
+          <div className="container-3">
+            <div className="line-3">
+              <div className="icon"></div>
+              <div className="line"></div>
+            </div>
+            <div className="text-3">
+              <h3>Never Miss Out</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
+            </div>
+          </div>
+
+          <div className="container-4">
+            <div className="line-4">
+              <div className="icon"></div>
+              <div className="line"></div>
+            </div>
+            <div className="text-4">
+              <h3>Quick Answers Chat</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
+            </div>
+          </div>
+
+
+        </div>
+
     </div>
   )
 }
